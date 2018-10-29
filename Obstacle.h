b20d3a6f02d7b8248b9a4ed9820b22a012310773
Orochi <guillaumefrezouls88@gmail.com>
2018-10-29 15:33:03 +0100

    qu'y a t-il dans une foret???

--- Obstacle.h
+++ Obstacle.h
@@ -1,18 +1,20 @@
+/*atom://teletype/portal/6fb2131e-b27a-4a17-8938-d3294b811301*/
+/*rajouter les ronces : le perso avance moins vite*/
+
 #ifndef OBSTACLE_H
 #define OBSTACLE_H
 
 class Obstacle{
 private:
+    int diametre, hauteur, x, y, PV;
 
 public:
-  Obstacle();
-  ~Obstacle();
-
-/*CECI EST UN TEST COMENTAIRE
-
-atom://teletype/portal/6fb2131e-b27a-4a17-8938-d3294b811301
-*/
-
+    Obstacle();
+    ~Obstacle();
+    virtual int getDiametre();
+    virtual int getHauteur();
+    virtual int getX();
+    virtual int getY();
+    virtual int getPV();
 };
-
 #endif
